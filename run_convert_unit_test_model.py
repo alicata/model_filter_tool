29359d461eb358be473ad02f2d045ff84385c380
alicata <alicata@github.com>
2023-09-11 23:38:48 -0700

    fix harcoded input_shape and retreive from get_model()

--- run_convert_unit_test_model.py
+++ run_convert_unit_test_model.py
@@ -1,5 +1,5 @@
 from mdl_converter import *
-from trainers.trainer_simple import AutoGeneratorTrainer
+from trainers.trainer_simple import *
 
 import torch
 import os
@@ -8,19 +8,34 @@ import sys
 
 os.makedirs("./output/", exist_ok=True)
 
-def get_network_definition():
-    return AutoGeneratorTrainer().model
+def get_model(torch_model_path):
+    """
+    get_model create network definition and loads weights into it
+
+    NOTE: for now tested and validated only with the unit test model 1d logistic regression
+    """
+
+    # TODO: input dimenions and shape should be dynamically retreived from the model structure
+    input_dims = 1
+
+    model = LogisticRegressionNetwork(input_dims)
+    input_shape = next(model.parameters()).shape
+    assert(input_shape[0]==input_dims)
+
+    model.load_state_dict(torch.load(torch_model_path))
+    print(model)
+    return model, input_shape
 
 print(f"---- {sys.argv[0]} ---- ")
 
 if (len(sys.argv)) > 1:
     torch_model_path = sys.argv[1]
-    torch_model = get_network_definition()
     onnx_model_path = torch_model_path.split(".pth")[0] + ".onnx"
 
+    torch_model, input_shape = get_model(torch_model_path)
+
     print(f"converting input model {torch_model_path} to {onnx_model_path} ...")
-    torch_model.load_state_dict(torch.load(torch_model_path))
-    export_torch_to_onnx(torch_model, onnx_model_path, shape=(1,), conversion_check_enabled=True)
+    export_torch_to_onnx(torch_model, onnx_model_path, shape=input_shape, conversion_check_enabled=True)
 else:
     print("error: no input model provided for convertion!")
     exit(0)
