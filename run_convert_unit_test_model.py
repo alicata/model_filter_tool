5199c906c0ce93480ee0d57ea0dba0c79b547ff9
alicata <alicata@github.com>
2023-09-12 18:41:29 -0700

    add pytorch default model compression

--- run_convert_unit_test_model.py
+++ run_convert_unit_test_model.py
@@ -4,6 +4,12 @@ from trainers.trainer_simple import *
 import torch
 import os
 import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_model', '-i', help='input torch model path')
+parser.add_argument('--output_model', '-o', help='output onnx model path')
+parser.add_argument('--compress', '-c', action='store_true', help='enable model compression')
 
 
 os.makedirs("./output/", exist_ok=True)
@@ -31,11 +37,19 @@ def get_model(torch_model_path):
 
 print(f"---- {sys.argv[0]} ---- ")
 
+
 if (len(sys.argv)) > 1:
-    torch_model_path = sys.argv[1]
+    # get torch model path from command line argument --input_model
+    args = parser.parse_args()
+    torch_model_path = args.input_model
+
     onnx_model_path = torch_model_path.split(".pth")[0] + ".onnx"
 
     torch_model, input_shape = get_model(torch_model_path)
+    if parser.parse_args().compress:
+        print("converting pytorch model to quantized model ...")
+        torch_model = torch.quantization.convert(torch_model, inplace=True)
+        torch_model.eval()
 
     print(f"converting input model {torch_model_path} to {onnx_model_path} ...")
     export_torch_to_onnx(torch_model, onnx_model_path, shape=input_shape, conversion_check_enabled=True)
