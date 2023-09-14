dcb78072d35569374db9d1aa4a9b69432520d305
alicata <alicata@users.noreply.github.com>
2023-09-13 23:21:07 -0700

    F bfloat16 model compression (#2)
    * fix input size to be detected automatically from model
    
    * add pytorch default model compression
    
    * fix quantized model output filename should be tagged as quant
    
    * add bfloa16 quantization of model, exporting to onnx, pth
    
    * fix message when ORT cannot load bfloat16 quantized model, because MatMul bfloat16 op not supported
    
    * add reference bfloat16 model
    
    ---------
    
    Co-authored-by: alicata <alicata@github.com>

--- mdl_converter.py
+++ mdl_converter.py
@@ -12,30 +12,49 @@ def load_onnx(onnx_model_path):
     onnx.checker.check_model(onnx_model)
     return onnx_model
 
+def get_model_quantization_type(torch_model):
+    return next(torch_model.parameters()).dtype
+
 def validate_model_inference(onnx_path, torch_out, x):
-    ort_session = onnxruntime.InferenceSession(onnx_path)
-    ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(x)}
-    ort_outs = ort_session.run(None, ort_inputs)
-    print("val: ort inputs : ", ort_inputs)
-    print("val: ort outputs: ", ort_outs)
-    np.testing.assert_allclose(to_numpy(torch_out), ort_outs[0], rtol=1e-03, atol=1e-05)
-    print("val: Exported model tested ONNXRuntime: prediction results all close to original. OK")
+    print(f"val: load onnx model {onnx_path} into memory ...")
+    try:
+        ort_session = onnxruntime.InferenceSession(onnx_path)
+        print("val: prepare input for ONNXRuntime inference")
+        ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(x.to('cuda').to(dtype=torch.float32))}
+        ort_outs = ort_session.run(None, ort_inputs)
+        print("val: ort inputs : ", ort_inputs)
+        print("val: ort outputs: ", ort_outs)
+        try:
+            np.testing.assert_allclose(to_numpy(torch_out), ort_outs[0], rtol=1e-03, atol=1e-05)
+            print("val: Exported model tested ONNXRuntime: prediction results all close to original. OK")
+        except AssertionError as e:
+            print("val: Exported model tested ONNXRuntime: prediction results not close to original. FAIL")
+            print(e)
+    except Exception as e:
+        print("     [VALIDATION ERROR] : Exported but onnx file was not validated because ORT cannot complete.")
+        print("    ", e)
+
 
 def export_torch_to_onnx(torch_model, onnx_model_path, shape=(3, 224, 224), conversion_check_enabled=False):
     # Input to the model, output a prediction
     print("export: switch model to eval mode (disable dropout and batchnorm)")
-    torch_model.eval()
 
     # Generate a batch with 1 random sample, and given shape
-    #x = torch.randn(1, shape[0], shape[1], shape[2], requires_grad=True)
     x = torch.randn(1, *shape, requires_grad=True)
+    device = next(torch_model.parameters()).device
+
+    if device.type == 'cuda':
+        print("export: model is on GPU, convert input to cuda tensor")
+        x = x.to('cuda')
+        if get_model_quantization_type(torch_model) == torch.bfloat16: 
+            x = x.to(dtype=torch.bfloat16)  
 
     print("export: convert the model in onnx format using tracing method: ", onnx_model_path)
     torch.onnx.export(torch_model,             # model being run
                     x,                         # model input (or a tuple for multiple inputs)
                     onnx_model_path,           # where to save the model (can be a file or file-like object)
                     export_params=True,        # store the trained parameter weights inside the model file
-                    opset_version=10,          # the ONNX version to export the model to
+                    opset_version=13,          # the ONNX version to export the model to
                     do_constant_folding=True,  # whether to execute constant folding for optimization
                     input_names = ['input'],   # the model's input names
                     output_names = ['output'], # the model's output names
--- run_convert_unit_test_model.py
+++ run_convert_unit_test_model.py
@@ -4,23 +4,32 @@ from trainers.trainer_simple import *
 import torch
 import os
 import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_model', '-i', help='input torch model path')
+parser.add_argument('--output_model', '-o', help='output onnx model path')
+parser.add_argument('--compress', '-c', action='store_true', help='enable model compression')
 
 
 os.makedirs("./output/", exist_ok=True)
 
 def get_model(torch_model_path):
+    """Create model from network definition and load weights into it
+
+    Args:
+        torch_model_path (str): path to the model weights
+
+    Returns:
+        model (nn.Module): network model loaded with the weights
+        input_shape (tuple): input shape of the model
+
     """
-    get_model create network definition and loads weights into it
+    model = LogisticRegressionNetwork(input_dims=1)
 
-    NOTE: for now tested and validated only with the unit test model 1d logistic regression
-    """
-
-    # TODO: input dimenions and shape should be dynamically retreived from the model structure
-    input_dims = 1
-
-    model = LogisticRegressionNetwork(input_dims)
+    # Get input shape from the model
     input_shape = next(model.parameters()).shape
-    assert(input_shape[0]==input_dims)
+    assert(input_shape[0]==1)
 
     model.load_state_dict(torch.load(torch_model_path))
     print(model)
@@ -28,18 +37,58 @@ def get_model(torch_model_path):
 
 print(f"---- {sys.argv[0]} ---- ")
 
+
 if (len(sys.argv)) > 1:
-    torch_model_path = sys.argv[1]
-    onnx_model_path = torch_model_path.split(".pth")[0] + ".onnx"
+    # get torch model path from command line argument --input_model
+    args = parser.parse_args()
+    torch_model_path = args.input_model
+    qtype = "bf16" # bf16 only supported, unsupported "fbgemm" or "qnnpack"
+    quantization_enabled = parser.parse_args().compress
+
 
     torch_model, input_shape = get_model(torch_model_path)
+    if quantization_enabled:
+        print("--------- model compression enabled -----------")
+        print(f"converting pytorch model to quantized format {qtype} ...")
+        if qtype == "fbgemm":
+            # quantize compress model to CPU-side int8
+            torch_model.qconfig = torch.quantization.get_default_qconfig('fbgemm')
+            torch.quantization.prepare(torch_model, inplace=True)
+            torch.quantization.convert(torch_model, inplace=True)
+        elif qtype == "qnnpack":
+            # quantize compress model to CPU-side int8
+            torch_model.qconfig = torch.quantization.get_default_qconfig('qnnpack')
+            torch.quantization.prepare(torch_model, inplace=True)
+            torch.quantization.convert(torch_model, inplace=True)
+        elif qtype == "bf16":
+            # quantize compress model to GPU-side bf16
+            torch_model = torch_model.to('cuda').to(dtype=torch.bfloat16)
+            input_data = torch.randn(*input_shape).to('cuda').to(dtype=torch.bfloat16)
+            torch.save(torch_model.state_dict(), torch_model_path.split(".pth")[0] + ".{qtype}.pth")
+            with torch.no_grad():
+                output = torch_model(input_data)    
+        onnx_model_path = torch_model_path.split(".pth")[0] + "." + str(qtype) + ".quant.onnx"
+    else:
+        onnx_model_path = torch_model_path.split(".pth")[0] + ".onnx"
+    torch_model.eval()
 
+    # report device and model parameters
+    device = next(torch_model.parameters()).device
+    print(f"----------{device.type},{device}---------------------")
+
+    for name, param in torch_model.named_parameters():
+        print(f"Parameter: {name}, dtype: {param.dtype} bytes: {param.element_size() * param.numel()}")
+    print("Parameters layers: ", sum(p.numel() for p in torch_model.parameters() if p.requires_grad))      
+ 
+    print("-------------------------------------------------")
     print(f"converting input model {torch_model_path} to {onnx_model_path} ...")
+    print("-------------------------------------------------")
     export_torch_to_onnx(torch_model, onnx_model_path, shape=input_shape, conversion_check_enabled=True)
 else:
     print("error: no input model provided for convertion!")
     exit(0)
 
-print(f"{sys.argv[0]} completed.")
+print("-------------------------------------------------")
+print(f"completed creating model {onnx_model_path}!")
 
 
